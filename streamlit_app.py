c4b11403832018dec37f24d1ed22e8b71672c999
Anees Ur Rehman <120562804+AneesUrRehman6@users.noreply.github.com>
2025-07-20 07:39:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 # import cv2
 # import mediapipe as mp
 # import numpy as np
-# import tensorflow as tf
+import tensorflow as tf
 # import time
 # from collections import deque
 
